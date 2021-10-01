c50c443645d0cd84923528f18c769d550d1c3dfb
bendunhampdx <bendunhampdx@gmail.com>
2021-10-01 11:29:26 -0700

    pass third order test returning an empty list

--- BusinessTracker/Models/Order.cs
+++ BusinessTracker/Models/Order.cs
@@ -5,9 +5,15 @@ namespace BusinessTracker.Models
   public class Order
   {
     public string Description { get; set; }
+    private static List<Order> _instances = new List<Order> { };
+    
     public Order(string description)
     {
       Description = description;
+    }
+    public static List<Order> GetAll()
+    {
+        return _instances;
     }
   }
 }
