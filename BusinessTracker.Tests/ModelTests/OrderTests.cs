e0094e49fee648114acb873bbcc81cc1826a2f8e
bendunhampdx <bendunhampdx@gmail.com>
2021-10-01 11:25:03 -0700

    pass second Order test returning description of order

--- BusinessTracker.Tests/ModelTests/OrderTests.cs
+++ BusinessTracker.Tests/ModelTests/OrderTests.cs
@@ -14,6 +14,19 @@ namespace BusinessTracker.Tests
       Order newOrder = new Order("test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "1 pastry";
+
+      //Act
+      Order newOrder = new Order(description);
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
 
   }
 }
