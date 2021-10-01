79c640984787d2cfd205f52a78ad02a78f88d3e0
bendunhampdx <bendunhampdx@gmail.com>
2021-10-01 14:23:39 -0700

    update index.cshtml pages for vendors and orders

--- BusinessTracker/Controllers/OrderController.cs
+++ BusinessTracker/Controllers/OrderController.cs
@@ -6,6 +6,29 @@ namespace BusinessOrganizer.Controllers
 {
   public class OrdersController : Controller
   {
+    [HttpGet("/venders/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendors vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
 
   }
 }
